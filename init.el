b3c9dda2e8a98612476b6cb7a5500e984c9eba0c
Ken Chow <kenchow@fosiao.com>
2015-08-25 09:53:45 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,384 @@
+
+;;
+;;C-c s s         Find symbol.
+;;C-c s d         Find global definition.
+;;C-c s g         Find global definition (alternate binding).
+;;C-c s G         Find global definition without prompting.
+;;C-c s c         Find functions calling a function.
+;;C-c s C         Find called functions (list functions called from a function).
+;;C-c s t         Find text string.
+;;C-c s e         Find egrep pattern.
+;;C-c s f         Find a file.
+;;C-c s i         Find files #including a file.
+;;  buffer
+;;C-c s b         Display *cscope* buffer.
+;;C-c s B         Auto display *cscope* buffer toggle.
+;;C-c s n         Next symbol.
+;;C-c s N         Next file.
+;;C-c s p         Previous symbol.
+;;C-c s P         Previous file.
+;;C-c s u         Pop mark.
+;;
+;;
+;;
+;; emacs kicker --- kick start emacs setup
+;; Copyright (C) 2010 Dimitri Fontaine
+;;
+;; Author: Dimitri Fontaine <dim@tapoueh.org>
+;; URL: https://github.com/dimitri/emacs-kicker
+;; Created: 2011-04-15
+;; Keywords: emacs setup el-get kick-start starter-kit
+;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
+;;
+;; This file is NOT part of GNU Emacs.
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.40"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'cl)				; common lisp goodies, loop
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil t)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://github.com/dimitri/el-get/raw/master/el-get-install.el")
+    (end-of-buffer)
+    (eval-print-last-sexp)))
+
+;; now either el-get is `require'd already, or have been `load'ed by the
+;; el-get installer.
+
+;; set local recipes
+(setq
+ el-get-sources
+ '((:name buffer-move			; have to add your own keys
+	  :after (progn
+		   (global-set-key (kbd "<C-S-up>")     'buf-move-up)
+		   (global-set-key (kbd "<C-S-down>")   'buf-move-down)
+		   (global-set-key (kbd "<C-S-left>")   'buf-move-left)
+		   (global-set-key (kbd "<C-S-right>")  'buf-move-right)))
+
+   (:name smex				; a better (ido like) M-x
+	  :after (progn
+		   (setq smex-save-file "~/.emacs.d/.smex-items")
+		   (global-set-key (kbd "M-x") 'smex)
+		   (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
+
+   (:name magit				; git meet emacs, and a binding
+	  :after (progn
+		   (global-set-key (kbd "C-x C-z") 'magit-status)))
+
+   (:name goto-last-change		; move pointer back to last change
+	  :after (progn
+		   ;; when using AZERTY keyboard, consider C-x C-_
+		   (global-set-key (kbd "C-x C-/") 'goto-last-change)))))
+
+;; now set our own packages
+(setq
+ my:el-get-packages
+ '(el-get				; el-get is self-hosting
+   escreen            			; screen for emacs, C-\ C-h
+   php-mode-improved			; if you're into php...
+   switch-window			; takes over C-x o
+   auto-complete			; complete as you type with overlays
+   yasnippet 				; powerful snippet mode
+   zencoding-mode			; http://www.emacswiki.org/emacs/ZenCoding
+   color-theme		                ; nice looking emacs
+   color-theme-tango                    ; check out color-theme-solarized
+   ecb                                  ; read dir and files
+   elfeed                               ; read news
+   ibuffer-vc                           ; C-x b ibuffer
+   tabbar                               ; tab bar
+   sr-speedbar                          ; xxx
+   xcscope                              ; c code 
+   igrep                                ; current file grep
+   highlight-symbol)) 
+
+;;
+;; Some recipes require extra tools to be installed
+;;
+;; Note: el-get-install requires git, so we know we have at least that.
+;;
+(when (ignore-errors (el-get-executable-find "cvs"))
+  (add-to-list 'my:el-get-packages 'emacs-goodies-el)) ; the debian addons for emacs
+
+(when (ignore-errors (el-get-executable-find "svn"))
+  (loop for p in '(psvn    		; M-x svn-status
+		   )
+	do (add-to-list 'my:el-get-packages p)))
+
+(setq my:el-get-packages
+      (append
+       my:el-get-packages
+       (loop for src in el-get-sources collect (el-get-source-name src))))
+
+;; install new packages and init already installed packages
+(el-get 'sync my:el-get-packages)
+
+(setq ring-bell-function 'ignore)
+
+;; on to the visual settings
+(setq inhibit-splash-screen t)		; no splash screen, thanks
+(line-number-mode 1)			; have line numbers and
+(column-number-mode 1)			; column numbers in the mode line
+(show-paren-mode 1)                     ; show {}[]()
+
+;; yes or no
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; default frame size
+(add-to-list 'default-frame-alist '(height . 63))
+(add-to-list 'default-frame-alist '(width . 150))
+
+;;time display  
+(display-time-mode 1)
+(setq display-time-24hr-format t)   
+(setq display-time-day-and-date t)
+(setq display-time-format "%H:%M")
+(setq display-time-interval 1)
+(setq display-time-use-mail-icon t)
+
+(setq-default cursor-type 'bar)         ; cursor shown as line
+
+(setq frame-title-format "Ken @ %f")    ; show current position on title
+
+(tool-bar-mode -1)			; no tool bar with icons
+(scroll-bar-mode -1)			; no scroll bars
+(unless (string-match "apple-darwin" system-configuration)
+  ;; on mac, there's always a menu bar drown, don't have it empty
+  (menu-bar-mode -1))
+
+;; choose your own fonts, in a system dependant way
+(if (string-match "apple-darwin" system-configuration)
+    (set-face-font 'default "Courier-13")
+  (set-face-font 'default "Monospace-10"))
+
+(global-hl-line-mode)			; highlight current line
+(global-linum-mode 1)			; add line numbers on the left
+
+;; avoid compiz manager rendering bugs
+(add-to-list 'default-frame-alist '(alpha . 100))
+
+;; copy/paste with C-c and C-v and C-x, check out C-RET too
+(cua-mode)
+
+;; under mac, have Command as Meta and keep Option for localized input
+(when (string-match "apple-darwin" system-configuration)
+  (setq mac-allow-anti-aliasing t)
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'none))
+
+;; Use the clipboard, pretty please, so that copy/paste "works"
+(setq x-select-enable-clipboard t)
+
+;; Navigate windows with M-<arrows>
+(windmove-default-keybindings 'meta)
+(setq windmove-wrap-around t)
+
+;; winner-mode provides C-<left> to get back to previous window layout
+(winner-mode 1)
+
+;; whenever an external process changes a file underneath emacs, and there
+;; was no unsaved changes in the corresponding buffer, just revert its
+;; content to reflect what's on-disk.
+(global-auto-revert-mode 1)
+
+;; M-x shell is a nice shell interface to use, let's make it colorful.  If
+;; you need a terminal emulator rather than just a shell, consider M-x term
+;; instead.
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; If you do use M-x term, you will notice there's line mode that acts like
+;; emacs buffers, and there's the default char mode that will send your
+;; input char-by-char, so that curses application see each of your key
+;; strokes.
+;;
+;; The default way to toggle between them is C-c C-j and C-c C-k, let's
+;; better use just one key to do the same.
+(require 'term)
+(define-key term-raw-map  (kbd "C-'") 'term-line-mode)
+(define-key term-mode-map (kbd "C-'") 'term-char-mode)
+
+;; Have C-y act as usual in term-mode, to avoid C-' C-y C-'
+;; Well the real default would be C-c C-j C-y C-c C-k.
+(define-key term-raw-map  (kbd "C-y") 'term-paste)
+
+;; use ido for minibuffer completion
+(require 'ido)
+(ido-mode t)
+(setq ido-save-directory-list-file "~/.emacs.d/.ido.last")
+(setq ido-enable-flex-matching t)
+(setq ido-use-filename-at-point 'guess)
+(setq ido-show-dot-for-dired t)
+(setq ido-default-buffer-method 'selected-window)
+
+;; default key to switch buffer is C-x b, but that's not easy enough
+;;
+;; when you do that, to kill emacs either close its frame from the window
+;; manager or do M-x kill-emacs.  Don't need a nice shortcut for a once a
+;; week (or day) action.
+(global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
+(global-set-key (kbd "C-x C-c") 'ido-switch-buffer)
+(global-set-key (kbd "C-x B") 'ibuffer)
+
+;; have vertical ido completion lists
+(setq ido-decorations
+      '("\n-> " "" "\n   " "\n   ..." "[" "]"
+	" [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]"))
+
+;; C-x C-j opens dired with the cursor right on the file you're editing
+(require 'dired-x)
+
+;; code style
+(setq-default c-indent-tabs-mode t     ; Pressing TAB should cause indentation
+	      c-indent-level 4         ; A TAB is equivilent to four spaces
+	      c-argdecl-indent 0       ; Do not indent argument decl's extra
+	      c-tab-always-indent t
+	      backward-delete-function nil) ; DO NOT expand tabs when deleting
+(c-add-style "my-c-style" '((c-continued-statement-offset 4))) ; If a statement continues on the next line, indent the continuation by 4
+(defun my-c-mode-hook ()
+  (c-set-style "my-c-style")
+  (c-set-offset 'substatement-open '0) ; brackets should be at same indentation level as the statements they open
+  (c-set-offset 'inline-open '+)
+  (c-set-offset 'block-open '+)
+  (c-set-offset 'brace-list-open '+)   ; all "opens" should be indented by the c-indent-level
+  (c-set-offset 'case-label '+))       ; indent case labels by c-indent-level, too
+(add-hook 'c-mode-hook 'my-c-mode-hook)
+(add-hook 'c++-mode-hook 'my-c-mode-hook)
+
+;; code format when copy from anywhere
+(dolist (command '(yank yank-pop))
+  (eval
+   `(defadvice ,command (after indent-region activate)
+      (and (not current-prefix-arg)
+           (member major-mode
+                   '(emacs-lisp-mode
+                     lisp-mode
+                     clojure-mode
+                     scheme-mode
+                     haskell-mode
+                     ruby-mode
+                     rspec-mode
+                     python-mode
+                     c-mode
+                     c++-mode
+                     objc-mode
+                     latex-mode
+                     js-mode
+                     plain-tex-mode))
+           (let ((mark-even-if-inactive transient-mark-mode))
+             (indent-region (region-beginning) (region-end) nil))))))
+
+
+;;elfeed
+(global-set-key (kbd "C-x w") 'elfeed)
+(setq elfeed-feeds
+      '("http://nullprogram.com/feed/"
+        "http://www.terminally-incoherent.com/blog/feed/"
+	"http://feed.cnblogs.com/kb/"))
+(setf url-queue-timeout 10)
+
+
+;; ibuffer replace of emace buffer
+(global-set-key (kbd "C-x b") 'ibuffer)
+
+
+;; tabbar
+(require 'tabbar)
+(setq tabbar-buffer-groups-function nil)
+(tabbar-mode)
+
+
+;; add a mark use C-., then come back with C-,
+(global-set-key [(control ?\.)] 'ska-point-to-register)
+(global-set-key [(control ?\,)] 'ska-jump-to-register)
+(defun ska-point-to-register()
+  "Store cursorposition _fast_ in a register. 
+Use ska-jump-to-register to jump back to the stored 
+position."
+  (interactive)
+  (setq zmacs-region-stays t)
+  (point-to-register 8))
+
+(defun ska-jump-to-register()
+  "Switches between current cursorposition and position
+that was stored with ska-point-to-register."
+  (interactive)
+  (setq zmacs-region-stays t)
+  (let ((tmp (point-marker)))
+    (jump-to-register 8)
+    (set-register 8 tmp)))
+
+
+;;highlight
+(require 'highlight-symbol)
+(highlight-symbol-mode 1)
+(global-set-key [(control f3)] 'highlight-symbol)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+
+;; erc
+(require 'erc)
+(setq erc-default-coding-system '(utf-8 . utf-8))
+(setq erc-nick "kenchowcn"
+      erc-user-full-name "Ken Chow")'
+
+;;(erc-autojoin-mode 1)
+;;(setq erc-autojoin-channels-alist
+;;      '(("irc.freenode.net"   ;
+;;         "#szdiy")))
+
+(setq erc-ignore-list nil)
+(setq erc-hide-list
+      '("JOIN" "PART" "QUIT" "MODE"))
+
+(erc-timestamp-mode 1)
+(setq erc-insert-timestamp-function 'erc-insert-timestamp-left)
+;;
+;;(erc-match-mode 1)
+;;(setq erc-keywords '("emms" "python"))
+;;(setq erc-pals '("rms"))
+
+;; scroll speed
+(defun up-slightly () (interactive) (scroll-up 3)) 
+(defun down-slightly () (interactive) (scroll-down 3)) 
+(global-set-key [mouse-4] 'down-slightly) 
+(global-set-key [mouse-5] 'up-slightly)
+
+;;spell checking
+(if (string-match "apple-darwin" system-configuration)
+    (setq ispell-program-name "/usr/local/bin/ispell")
+  (setq ispell-program-name "/usr/bin/ispell"))
+
+
+;;cscope
+(require 'xcscope)
+;; only load on c-mode
+(add-hook 'c-mode-common-hook
+	  '(lambda ()
+	     (require 'xcscope)))
+
+
+;; key bind, this part should put into the end of configuration.
+;; in case conflicts happened
+
+(defun fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen
+		       (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
+(global-set-key [f11] 'fullscreen)         ; full screen
+
+(global-set-key [f9] 'sr-speedbar-toggle)  ; sr-speed-bar
+(global-set-key [f8] 'igrep)               ; local path grep
